11e6cedace84143e274ad06b4e2c712bd46a8d3c
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-02-16 13:38:01 -0300

    Creation of route layouts to start working on the visual structure of the pages.

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,8 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './app/dashboard'
 import { SignIn } from './auth/sign-in'
+import { AppLayout } from './_layouts/app'
 
 export const router = createBrowserRouter([
-    { path: '/', element: <Dashboard /> },
-    { path: '/sign-in', element: <SignIn /> },
+    {
+        path: '/',
+        element: <AppLayout />,
+        children: [
+            { path: '/', element: <Dashboard /> },
+        ]
+    },
+
+    {
+        path: '/',
+        element: <AppLayout />,
+        children: [
+            { path: '/sign-in', element: <SignIn /> },
+        ]
+    }
+
 ])
