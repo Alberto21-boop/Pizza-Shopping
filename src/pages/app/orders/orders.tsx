711775a8ebd46e1aece2f2346e098313f372ec50
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-03-01 08:35:02 -0300

    The order screen is nearly completed, the table with all fields is ready, along with its other components, however, I am facing a serious styling issue.

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,40 +1,83 @@
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table';
+import { ArrowRight, Search, X } from 'lucide-react';
 import { Helmet } from 'react-helmet-async';
 
 export function Orders() {
-    return(
+    return (
         <>
-          <Helmet title='Pedidos'/>
-          <div className='flex flex-col gap-4'>
-            <h1 className='text-3xl font-bold tracking-tight'>Pedidos</h1>
-          </div>
-          <div className='space-y-2.5'>
-            <form className='flex items-center gap-2'>
-                <span className='text-sm font-semibold'>Filtros:</span>
-                <Input placeholder='Nome do cliente' className='h-8 w-[320px]' />
-            </form>
-            <div className='border rounded-md'>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                         <TableHead>Identificador</TableHead>
-                         <TableHead>Realizado há</TableHead>
-                         <TableHead>Status</TableHead>
-                         <TableHead>Cliente</TableHead>
-                         <TableHead>Total do pedido</TableHead>
-                         <TableHead></TableHead>
-                         <TableHead></TableHead>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell></TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
+            <Helmet title='Pedidos' />
+            <div className='flex flex-col gap-4'>
+                <h1 className='text-3xl font-bold tracking-tight'>Pedidos</h1>
             </div>
-          </div>
+            <div className='space-y-2.5'>
+                <form className='flex items-center gap-2'>
+                    <span className='text-sm font-semibold'>Filtros:</span>
+                    <Input placeholder='Nome do cliente' className='h-8 w-[320px]' />
+                </form>
+
+                <div className='rounded-md border'>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableHead className='w-[64px]'></TableHead>
+                                <TableHead className='w-[140px]'>Identificador</TableHead>
+                                <TableHead className='w-[180px]'>Realizado há</TableHead>
+                                <TableHead className='w-[140px]'>Status</TableHead>
+                                <TableHead>Cliente</TableHead>
+                                <TableHead className='w-[140px]'>Total do pedido</TableHead>
+                                <TableHead className='w-[164px]'></TableHead>
+                                <TableHead className='w-[132px]'></TableHead>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>
+                                    <Button variant='outline' size='xs'>
+                                        <Search className='h-3 w-3' />
+                                        <span className='sr-only'>Detalhes do pedido</span>
+                                    </Button>
+                                    <TableCell>
+                                        <TableCell className='font-mono text-xs font-medium'>
+                                            456ds4fd6s4f656ds
+                                        </TableCell>
+                                        <TableCell className=' text-muted-foreground'>
+                                            há 15 minutos
+                                        </TableCell>
+                                        <TableCell>
+                                            <div className='flex items-center gap-2'>
+                                                <span className='h-2 w-2 rounded-full bg-slate-400' />
+                                                <span className='font-medium text-muted-foreground'>
+                                                    Pendente
+                                                </span>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell className='font-medium'>
+                                            Alberto Barbosa
+                                        </TableCell>
+                                        <TableCell className='font-medium'>
+                                            R$ 194,90
+                                        </TableCell>
+                                        <TableCell>
+                                        <Button variant='outline' size='xs'>
+                                                <ArrowRight className='h-3 w-3 mr-2' />
+                                                Aprovar
+                                            </Button>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Button variant='ghost' size='xs'>
+                                                <X className='h-3 w-3 mr-2' />
+                                                Cancelar
+                                            </Button>
+                                        </TableCell>
+                                    </TableCell>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
         </>
     )
 }
