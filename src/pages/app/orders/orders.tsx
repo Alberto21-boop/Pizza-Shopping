69b545a909da5dd53dc8513106b6b17ef0739df7
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-03-03 10:33:31 -0300

    Resolving table styling issue and aligning the table header.

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { ArrowRight, Search, X } from 'lucide-react';
 import { Helmet } from 'react-helmet-async';
 
@@ -17,12 +17,12 @@ export function Orders() {
                     <Input placeholder='Nome do cliente' className='h-8 w-[320px]' />
                 </form>
 
-                <div className='rounded-md border'>
+                <div className='border rounded-md'>
                     <Table>
-                        <TableHead>
+                        <TableHeader>
                             <TableRow>
                                 <TableHead className='w-[64px]'></TableHead>
-                                <TableHead className='w-[140px]'>Identificador</TableHead>
+                                <TableHead className='w-[140px] pl-8'>Identificador</TableHead>
                                 <TableHead className='w-[180px]'>Realizado há</TableHead>
                                 <TableHead className='w-[140px]'>Status</TableHead>
                                 <TableHead>Cliente</TableHead>
@@ -30,7 +30,7 @@ export function Orders() {
                                 <TableHead className='w-[164px]'></TableHead>
                                 <TableHead className='w-[132px]'></TableHead>
                             </TableRow>
-                        </TableHead>
+                        </TableHeader>
                         <TableBody>
                             <TableRow>
                                 <TableCell>
@@ -38,40 +38,40 @@ export function Orders() {
                                         <Search className='h-3 w-3' />
                                         <span className='sr-only'>Detalhes do pedido</span>
                                     </Button>
-                                    <TableCell>
-                                        <TableCell className='font-mono text-xs font-medium'>
-                                            456ds4fd6s4f656ds
-                                        </TableCell>
-                                        <TableCell className=' text-muted-foreground'>
-                                            há 15 minutos
-                                        </TableCell>
-                                        <TableCell>
-                                            <div className='flex items-center gap-2'>
-                                                <span className='h-2 w-2 rounded-full bg-slate-400' />
-                                                <span className='font-medium text-muted-foreground'>
-                                                    Pendente
-                                                </span>
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className='font-medium'>
-                                            Alberto Barbosa
-                                        </TableCell>
-                                        <TableCell className='font-medium'>
-                                            R$ 194,90
-                                        </TableCell>
-                                        <TableCell>
-                                        <Button variant='outline' size='xs'>
-                                                <ArrowRight className='h-3 w-3 mr-2' />
-                                                Aprovar
-                                            </Button>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Button variant='ghost' size='xs'>
-                                                <X className='h-3 w-3 mr-2' />
-                                                Cancelar
-                                            </Button>
-                                        </TableCell>
+                                </TableCell>
+                                <TableCell>
+                                    <TableCell className='font-mono text-xs font-medium'>
+                                        456ds4fd6s4f656ds
                                     </TableCell>
+                                </TableCell>
+                                <TableCell className=' text-muted-foreground'>
+                                    há 15 minutos
+                                </TableCell>
+                                <TableCell>
+                                    <div className='flex items-center gap-2'>
+                                        <span className='h-2 w-2 rounded-full bg-slate-400' />
+                                        <span className='font-medium text-muted-foreground'>
+                                            Pendente
+                                        </span>
+                                    </div>
+                                </TableCell>
+                                <TableCell className='font-medium'>
+                                    Alberto Barbosa
+                                </TableCell>
+                                <TableCell className='font-medium'>
+                                    R$ 194,90
+                                </TableCell>
+                                <TableCell>
+                                    <Button variant='outline' size='xs'>
+                                        <ArrowRight className='h-3 w-3 mr-2' />
+                                        Aprovar
+                                    </Button>
+                                </TableCell>
+                                <TableCell>
+                                    <Button variant='ghost' size='xs'>
+                                        <X className='h-3 w-3 mr-2' />
+                                        Cancelar
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                         </TableBody>
