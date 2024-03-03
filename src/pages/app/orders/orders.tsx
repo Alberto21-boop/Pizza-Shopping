c82b7b810c3510a89794c1cafc2c086f2d1fc806
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-03-03 12:38:04 -0300

    Pagination completed, and adding buttons to interact with pagination.

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,7 +1,8 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Helmet } from 'react-helmet-async';
 import { OrderTableRow } from './order-table-row';
 import { OrderTableFilter } from './order-table-filters';
+import { Pagination } from '@/components/pagination';
 
 export function Orders() {
     return (
@@ -9,7 +10,7 @@ export function Orders() {
             <Helmet title='Pedidos' />
             <div className='flex flex-col gap-4'>
                 <h1 className='text-3xl font-bold tracking-tight'>Pedidos</h1>
-            </div>
+            
             <div className='space-y-2.5'>
                 <OrderTableFilter />
 
@@ -35,6 +36,9 @@ export function Orders() {
                             })}
                         </TableBody>
                     </Table>
+                    </div> 
+
+                    <Pagination pageIndex={0} totalCount={105} perPage={10}/> 
                 </div>
             </div>
         </>
