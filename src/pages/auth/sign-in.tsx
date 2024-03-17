80480ba4e485ec627b916b6c57ca0e82bf751391
Alberto21-boop <albertobarbosa0003@gmail.com>
2024-03-17 13:59:50 -0300

    Building the file that will handle authentication for POST requests to the backend.

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,8 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from 'sonner'
 import { Link } from 'react-router-dom'
+import { useMutation } from '@tanstack/react-query';
+import { signIn } from '@/api/sign-in';
 
 const signInForm = z.object({
   email: z.string().email()
@@ -16,10 +18,13 @@ type signInForm = z.infer<typeof signInForm>
 export function SignIn() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<signInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: signInForm) {
     try {
-      console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+     await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
